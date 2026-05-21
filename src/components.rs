c216aacc60209a964a0e5201ceda260fd0db4501
maierlekt <maierle.kt@gmail.com>
2026-05-22 01:35:00 +0200

    main: init commit

---
+++ src/components.rs
@@ -0,0 +1,3 @@
+pub struct Position { pub x: i32, pub y: i32 }
+pub struct Renderable { pub glyph: char }
+pub struct Player;
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+mod components;
+mod render;
+mod input;
+
+use crossterm::{
+    cursor, execute,
+    terminal::{self},
+};
+use hecs::World;
+use std::io::stdout;
+use components::{Position, Renderable, Player};
+
+fn main() -> std::io::Result<()> {
+    let mut stdout = stdout();
+    terminal::enable_raw_mode()?;
+    execute!(stdout, terminal::EnterAlternateScreen, cursor::Hide)?;
+
+    let mut world = World::new();
+    world.spawn((Position { x: 10, y: 10 }, Renderable { glyph: '@' }, Player));
+
+    loop {
+        render::render(&mut stdout, &world)?;
+
+        if !input::read_input(&mut world)? {
+            break;
+        }
+
+
+    }
+
+    execute!(stdout, terminal::LeaveAlternateScreen, cursor::Show)?;
+    terminal::disable_raw_mode()?;
+    Ok(())
 }
