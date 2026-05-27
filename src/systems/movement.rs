26f150caa10ae7343527923809950aa410a880b1
idontknowhowbut <a.fedorov45123@gmail.com>
2026-05-27 20:13:43 +0300

    develop: oaaaa

--- src/systems/movement.rs
+++ src/systems/movement.rs
@@ -1,7 +1,5 @@
 use hecs::World;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 use crate::components::{Player, Position};
 
 pub fn process_player_movement(world: &mut World, dx: i32, dy: i32) {
@@ -10,16 +8,3 @@ pub fn process_player_movement(world: &mut World, dx: i32, dy: i32) {
         pos.y += dy;
     }
 }
-=======
-pub fn process_movement(world: &mut World, dx: i32, dy: i32) {}
->>>>>>> 2e8e1a0 (develop: refactor input, add basic movement sys)
-=======
-use crate::components::{Player, Position};
-
-pub fn process_player_movement(world: &mut World, dx: i32, dy: i32) {
-    for (_entity, (_player, pos)) in world.query_mut::<(&Player, &mut Position)>() {
-        pos.x += dx;
-        pos.y += dy;
-    }
-}
->>>>>>> ed5b3f5 (develop: iterate movement)
