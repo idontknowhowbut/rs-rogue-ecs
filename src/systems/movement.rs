0acf155faf561cca5ec782f17ab1a64e3a8a89c7
idontknowhowbut <a.fedorov45123@gmail.com>
2026-05-27 14:01:31 +0300

    develop: implement basic movement

--- src/systems/movement.rs
+++ src/systems/movement.rs
@@ -1,9 +1,10 @@
-use hecs::{World};
+use hecs::World;
 
 use crate::components::{Player, Position};
 
 pub fn process_player_movement(world: &mut World, dx: i32, dy: i32) {
-    if let Some(n) = world.query_mut()::<(&mut Position, &Player)>  {
-
-    };
+    for (_entity, (_player, pos)) in world.query_mut::<(&Player, &mut Position)>() {
+        pos.x += dx;
+        pos.y += dy;
+    }
 }
