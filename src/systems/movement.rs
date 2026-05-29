b17d01e88e694ecf687bb45b57193802e0f8f6ae
idontknowhowbut <a.fedorov45123@gmail.com>
2026-05-29 16:00:10 +0300

    develop: add wall collision for movement sys

--- src/systems/movement.rs
+++ src/systems/movement.rs
@@ -1,10 +1,22 @@
 use hecs::World;
 
-use crate::components::{Player, Position};
+use crate::{components::{Player, Position}, map::{Map, TileType, idx_map}};
 
-pub fn process_player_movement(world: &mut World, dx: i32, dy: i32) {
+pub fn process_player_movement(world: &mut World, dx: i32, dy: i32, map: &Map) {
+    let mut new_x: i32;
+    let mut new_y: i32;
     for (_entity, (_player, pos)) in world.query_mut::<(&Player, &mut Position)>() {
-        pos.x += dx;
-        pos.y += dy;
+        new_x = pos.x + dx;
+        new_y = pos.y + dy;
+        let target_tile = &map.map_field[idx_map(new_x, new_y, map.map_width)];
+        match target_tile {
+            TileType::Floor => {
+                pos.x = new_x;
+                pos.y = new_y;
+            },
+            TileType::Wall => {
+                return
+            }
+        }
     }
 }
--- src/systems/render.rs
+++ src/systems/render.rs
@@ -2,7 +2,7 @@ use crate::{
     components::{Position, Renderable},
     map::{Map, TileType},
 };
-use crossterm::{cursor, queue};
+use crossterm::{cursor, execute, queue, style::{Color, ResetColor, SetBackgroundColor, SetForegroundColor}};
 use hecs::World;
 use std::io::Stdout;
 use std::io::Write;
@@ -17,20 +17,22 @@ pub fn render(stdout: &mut Stdout, world: &World, map: &Map) -> std::io::Result<
 
 fn render_entities(stdout: &mut Stdout, world: &World) {
     for (_, (pos, render)) in world.query::<(&Position, &Renderable)>().iter() {
-        let _ = queue!(stdout, cursor::MoveTo(pos.x as u16, pos.y as u16));
+        _ = queue!(stdout, SetForegroundColor(Color::DarkGreen), cursor::MoveTo(pos.x as u16, pos.y as u16));
         _ = write!(stdout, "{}", render.glyph);
+        _ = execute!(stdout, ResetColor);
     }
+    
 }
 
 fn render_map(stdout: &mut Stdout, map: &Map) {
     let mut y: i32 = 0;
 
-    while map.map_width + map.map_width * y <= map.map_field.len() as i32 {
+    while y < map.map_height {
         let mut map_row: Vec<char> = Vec::new();
 
-        for tile in &map.map_field
-            [(map.map_width * y) as usize..(map.map_width + map.map_width * y) as usize]
-        {
+        let row_start = (map.map_width * y) as usize;
+        let row_end = row_start + map.map_width as usize;
+        for tile in &map.map_field[row_start..row_end] {
             match *tile {
                 TileType::Floor => map_row.push('.'),
                 TileType::Wall => map_row.push('#'),
