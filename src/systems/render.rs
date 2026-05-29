8a34258d8193e2f59183909aa3305e93d6a7b302
idontknowhowbut <a.fedorov45123@gmail.com>
2026-05-29 09:55:22 +0300

    develop: iterate render

--- src/systems/render.rs
+++ src/systems/render.rs
@@ -2,9 +2,7 @@ use crate::{
     components::{Position, Renderable},
     map::{Map, TileType},
 };
-use crossterm::{
-    cursor, queue,
-};
+use crossterm::{cursor, queue};
 use hecs::World;
 use std::io::Stdout;
 use std::io::Write;
@@ -25,21 +23,24 @@ fn render_entities(stdout: &mut Stdout, world: &World) {
 }
 
 fn render_map(stdout: &mut Stdout, map: &Map) {
-    let mut x: i32 = 0;
     let mut y: i32 = 0;
-    let mut map_row: Vec<char> = Vec::new();
 
-    for tile in &map.map_field[(map.map_width*y) as usize..(x+map.map_width*y) as usize] {
-        let glyph: char;
-        match *tile {
-            TileType::Floor => map_row.push('.'),
-            TileType::Wall =>  map_row.push('#'),
+    while map.map_width + map.map_width * y <= map.map_field.len() as i32 {
+        let mut map_row: Vec<char> = Vec::new();
+
+        for tile in &map.map_field
+            [(map.map_width * y) as usize..(map.map_width + map.map_width * y) as usize]
+        {
+            match *tile {
+                TileType::Floor => map_row.push('.'),
+                TileType::Wall => map_row.push('#'),
+            }
+
         }
 
-        x += 1;
-        if x == map.map_width {
-            y += 1;
-            x = 0;
-        }
+        let s: String = map_row.into_iter().collect();
+        let _ = queue!(stdout, cursor::MoveTo(0, y as u16));
+        _ = write!(stdout, "{}", s);
+        y += 1;
     }
 }
